a1bdc3a549b8e6cb9e326b9a78bd2f67f448d910
Alexandre <jalex.kam@gmail.com>
2023-09-20 16:59:09 +0100

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -2,7 +2,7 @@
 resource "aws_vpc" "vpc" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_support   = true
-  enable_dns_hostnames = true
+  enable_dns_hostnames = false
   tags                 = merge(local.common-tag, { Name : "${local.common-tag.environment}-vpc" })
 }
 
