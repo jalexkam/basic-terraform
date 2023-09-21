b221ce92b05845699a29dae638ef6831e945ca2a
Alexandre <jalex.kam@gmail.com>
2023-09-21 16:19:41 +0100

    Update igw.tf

--- igw.tf
+++ igw.tf
@@ -1,4 +1,4 @@
-resource "aws_internet_gateway" "int_gw2" {
+resource "aws_internet_gateway" "int_gw23" {
   vpc_id = aws_vpc.vpc.id
   tags   = merge(local.common-tag, { Name : "${local.common-tag.environment}-igw" })
 
