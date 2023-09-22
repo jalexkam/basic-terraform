421ba2c45d73962b92945233d832e3587aee351d
Alexandre <jalex.kam@gmail.com>
2023-09-22 16:24:33 +0100

    state reset

--- provider.tf
+++ provider.tf
@@ -3,17 +3,23 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
+      version = "5.17"
     }
   }
   backend "local" {
     path = "./terraformstate.tfstate"
   }
+  # backend "s3" {
+  #   key            = "basic-terraform/terraform.tfstate"
+  #   bucket         = "state-bucket-62746"
+  #   dynamodb_table = "state-table-62746"
+  #   encrypt        = true
+  #   region         = "us-east-1"
+  # }
 }
 
 # Configure the AWS Provider
 provider "aws" {
-  region  = "eu-west-1"
-  profile = "alex-meli-card-admincli"
-
+  region  = var.aws_region
+  profile = var.aws_profile
 }
