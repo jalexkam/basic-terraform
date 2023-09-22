36391682b787441c50eee30bd6c698bb8932bd6d
Alexandre <jalex.kam@gmail.com>
2023-09-22 17:07:10 +0100

    backend

--- provider.tf
+++ provider.tf
@@ -6,16 +6,16 @@ terraform {
       version = "5.17"
     }
   }
-  backend "local" {
-    path = "./terraformstate.tfstate"
-  }
-  # backend "s3" {
-  #   key            = "basic-terraform/terraform.tfstate"
-  #   bucket         = "state-bucket-62746"
-  #   dynamodb_table = "state-table-62746"
-  #   encrypt        = true
-  #   region         = "us-east-1"
+  # backend "local" {
+  #   path = "./terraformstate.tfstate"
   # }
+  backend "s3" {
+    key            = "basic-terraform/terraform.tfstate"
+    bucket         = "state-table-62746"
+    dynamodb_table = "state-table-62746"
+    encrypt        = true
+    region         = "us-east-1"
+  }
 }
 
 # Configure the AWS Provider
