cb67daa5707d1d607ad79752ee6c71100f636881
Alexandre <jalex.kam@gmail.com>
2023-09-26 00:37:10 +0100

    re-added provider

--- provider.tf
+++ provider.tf
@@ -21,8 +21,8 @@ terraform {
 
 # Configure the AWS Provider
 provider "aws" {
-  #  profile = "alex-meli-card-admincli"
-  region     = "us-east-1"
-  access_key = "AKIA5S2A5T5737JB7OPU"
-  secret_key = "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"
+  profile = "alex-meli-card-admincli"
+  region  = "us-east-1"
+  #access_key = "AKIA5S2A5T5737JB7OPU"
+  #secret_key = "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"
 }
