acb2e40a920885e5a361d04c655e617ce1f15ffa
Alexandre <jalex.kam@gmail.com>
2023-09-25 22:55:59 +0100

    another

--- provider.tf
+++ provider.tf
@@ -19,25 +19,6 @@ terraform {
   }
 }
 
-terraform {
-  required_version = ">= 0.13.1"
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
-    }
-
-    random = {
-      source  = "hashicorp/random"
-      version = ">= 2.0"
-    }
-  }
-}
-
-
-
-
 # Configure the AWS Provider
 provider "aws" {
   region  = var.aws_region
