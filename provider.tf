849c6c8ea3b1d212c9fdd712695abf657ece9ef0
Alexandre <jalex.kam@gmail.com>
2023-09-25 22:54:09 +0100

    another

--- provider.tf
+++ provider.tf
@@ -1,13 +1,25 @@
-# terraform {
-#   required_version = ">= 1.0"
-#   required_providers {
-#     aws = {
-#       source  = "hashicorp/aws"
-#       version = "5.17"
-#     }
-#   }
+terraform {
+  required_version = ">= 1.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.17"
+    }
+  }
 
-  terraform {
+  # backend "local" {
+  #   path = "./terraformstate.tfstate"
+  # }
+  backend "s3" {
+    key            = "basic-terraform/terraform.tfstate"
+    bucket         = "state-bucket-62746"
+    dynamodb_table = "state-table-62746"
+    encrypt        = true
+    region         = "us-east-1"
+  }
+}
+
+terraform {
   required_version = ">= 0.13.1"
 
   required_providers {
@@ -25,17 +37,6 @@
 
 
 
-  # backend "local" {
-  #   path = "./terraformstate.tfstate"
-  # }
-  backend "s3" {
-    key            = "basic-terraform/terraform.tfstate"
-    bucket         = "state-bucket-62746"
-    dynamodb_table = "state-table-62746"
-    encrypt        = true
-    region         = "us-east-1"
-  }
-}
 
 # Configure the AWS Provider
 provider "aws" {
