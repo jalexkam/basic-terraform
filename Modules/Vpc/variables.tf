9bc151e643331863db70036e2693f11b62c7d699
Alexandre <jalex.kam@gmail.com>
2023-09-26 11:59:45 +0100

    another

--- Modules/Vpc/variables.tf
+++ Modules/Vpc/variables.tf
@@ -1,9 +1,11 @@
 variable "aws_profile" {
   description = "AWS Profile"
   type        = string
+  default     = "us-east-1"
 }
 
 variable "aws_region" {
   description = "aws region"
   type        = string
+  default     = "alex-meli-card-admincli"
 }
