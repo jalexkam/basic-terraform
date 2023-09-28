797ed642dc214082fd427e2ca1f196c7c65d3c60
Alexandre <jalex.kam@gmail.com>
2023-09-28 14:10:55 +0100

    module added

---
+++ main.tf
@@ -0,0 +1,38 @@
+terraform {
+  required_version = ">= 1.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.17"
+    }
+  }
+
+  backend "s3" {
+    key            = "basic-terraform-reset/terraform.tfstate"
+    bucket         = "state-bucket-62746"
+    dynamodb_table = "state-table-62746"
+    encrypt        = true
+    region         = "us-east-1"
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region  = var.aws_region
+  profile = var.aws_profile
+  //shared_credentials_files = ["/home/ec2-user/.aws/credentials"]
+}
+
+
+module "vpc" {
+  source = "./vpc"
+
+  aws_profile = var.aws_profile
+  aws_region  = var.aws_region
+  environment = var.environment
+  location    = var.location
+
+  providers = {
+    aws = aws
+  }
+}
--- vpc/provider.tf
+++ vpc/provider.tf
@@ -6,19 +6,5 @@ terraform {
       version = "5.17"
     }
   }
-
-  backend "s3" {
-    key            = "basic-terraform-reset/terraform.tfstate"
-    bucket         = "state-bucket-62746"
-    dynamodb_table = "state-table-62746"
-    encrypt        = true
-    region         = "us-east-1"
-  }
 }
 
-# Configure the AWS Provider
-provider "aws" {
-  region                   = var.aws_region
-  profile                  = var.aws_profile
-  shared_credentials_files = ["/home/ec2-user/.aws/credentials"]
-}
