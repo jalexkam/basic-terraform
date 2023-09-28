39f69ef5e5074957b335f2e5700c25f588e230eb
Alexandre <jalex.kam@gmail.com>
2023-09-28 14:58:30 +0100

    added

--- production.tfvars
+++
@@ -1,5 +0,0 @@
-aws_region  = "us-east-1"
-aws_profile = "alex-meli-card-admincli"
-
-environment = "Production"
-location    = "USA"
--- vpc/locals.tf
+++
@@ -1,7 +0,0 @@
-locals {
-  common-tag = {
-    environment : var.environment,
-    project : "Fox"
-    location : var.location
-  }
-}
